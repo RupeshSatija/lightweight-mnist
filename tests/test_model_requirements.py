e34f2430112a27c5d4f3f015c0fbb27a4c9cdf2f
Rupesh <rupesh.satija@gmail.com>
2024-11-29 08:33:13 +1100

    update the test case

--- tests/test_model_requirements.py
+++ tests/test_model_requirements.py
@@ -5,7 +5,6 @@ from torch.utils.data import DataLoader
 from src.config.config import Config
 from src.data.dataset import get_mnist_dataset
 from src.models.mnist_cnn import MnistCNN
-from src.training.trainer import Trainer
 
 
 def count_parameters(model):
@@ -17,6 +16,7 @@ def test_model_size():
     config = Config.get_instance()
     model = MnistCNN(config)
     param_count = count_parameters(model)
+    print(f"Model has {param_count} parameters")
     assert (
         param_count < 25000
     ), f"Model has {param_count} parameters, should be less than 25000"
@@ -28,19 +28,31 @@ def test_model_accuracy():
     config = Config.get_instance()
     model = MnistCNN(config)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=Config.LEARNING_RATE)
-    trainer = Trainer(model, criterion, optimizer, device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     # Get data
     train_dataset = get_mnist_dataset(root="./data", train=True)
     test_dataset = get_mnist_dataset(root="./data", train=False)
 
-    train_loader = DataLoader(train_dataset, batch_size=Config.BATCH_SIZE, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=Config.BATCH_SIZE)
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64)
 
     # Train for 1 epoch
-    trainer.train(train_loader, test_loader, num_epochs=1)
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % 100 == 0:
+            print(
+                f"Train Batch: {batch_idx}/{len(train_loader)} Loss: {loss.item():.6f}"
+            )
 
     # Evaluate
     model.eval()
@@ -48,12 +60,13 @@ def test_model_accuracy():
     total = 0
 
     with torch.no_grad():
-        for images, labels in test_loader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            _, predicted = torch.max(output.data, 1)
+            total += target.size(0)
+            correct += (predicted == target).sum().item()
 
     accuracy = 100 * correct / total
+    print(f"Final accuracy: {accuracy}%")
     assert accuracy > 95, f"Model accuracy is {accuracy}%, should be >95%"
